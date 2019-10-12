75f13d094e92184332e876f4b93161d631f810f1
tuppers360 <tuppers360@live.co.uk>
2019-10-12 16:15:10 +0100

    moved our directory and shop data into its own reducer, created respe… … …ctive selectors and updated corresponding components with new redux flow for directory/shop.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,12 @@
 import React from "react";
+import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 
-import SHOP_DATA from "./shop.data";
-
-import CollectionPreview from "./../../components/collection-preview/collection-preview.component";
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherProps }) => (
-          <CollectionPreview key={id} {...otherProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherProps }) => (
+      <CollectionOverview key={id} {...otherProps} />
+    ))}
+  </div>
+);
 
 export default ShopPage;
