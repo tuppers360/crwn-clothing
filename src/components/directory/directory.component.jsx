0b7435e673957384195905f5a97a7c9f6e309aaf
tuppers360 <tuppers360@live.co.uk>
2019-10-12 19:53:41 +0100

    moved our directory and shop data into its own reducer, created respective selectors  and updated corresponding component with new redux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-
-import MenuItem from "./../menu-item/menu-item.component";
-
 import { createStructuredSelector } from "reselect";
+
 import { selectDirectorySections } from "../../redux/directory/directory.selector";
+
+import MenuItem from "../menu-item/menu-item.component";
 
 import "./directory.styles.scss";
 
 const Directory = ({ sections }) => (
   <div className="directory-menu">
-    {sections.map(({ title, imageUrl, id, size }) => (
-      <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
     ))}
   </div>
 );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import CollectionOverview from "../../components/collections-overview/collections-overview.component";
+import { Route } from "react-router-dom";
 
-const ShopPage = ({ collections }) => (
-  <div className="shop-page">
-    {collections.map(({ id, ...otherProps }) => (
-      <CollectionOverview key={id} {...otherProps} />
-    ))}
-  </div>
-);
+import CollectionsOverview from "./../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
+
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
