80d5746e6b70ae20e3f81ea827f794e67fa13ebb
tuppers360 <tuppers360@live.co.uk>
2019-11-01 21:56:59 +0000

    div style not required

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,10 +1,5 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-
-const OptionContainersStyles = css`
-  padding: 10px 15px;
-  cursor: pointer;
-`;
 
 export const HeaderContainer = styled.div`
   height: 70px;
@@ -29,8 +24,6 @@ export const OptionsContainer = styled.div`
 `;
 
 export const OptionLink = styled(Link)`
-  ${OptionContainersStyles}
-`;
-export const OptionDiv = styled(Link)`
-  ${OptionContainersStyles}
+  padding: 10px 15px;
+  cursor: pointer;
 `;
